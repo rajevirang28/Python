140dbefa1059cbbe1db4ae024151ee0d6a5eea15
rajevirang28 <158153320+rajevirang28@users.noreply.github.com>
2024-11-27 10:11:17 +0000

    pratice

---
+++ 12_solution.py
@@ -0,0 +1,8 @@
+number = int(input("Enter the number: "))
+is_prime = True
+if number > 1:
+    for i in range (2, number):
+        if(number % i) == 0:
+            is_prime = False
+            break
+print(is_prime)
